b4df034d2d72c113ca87e3d274a6d7ad41db9495
trossman <haticebucurgat@gmail.com>
2025-07-26 21:49:02 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,37 +1,67 @@
 import discord
 from discord.ext import commands
+from config import token  # Botun tokenini config dosyasından içe aktarma
+
 import random
 
-helloMessages = ["hi", "hello", "yo", "howdy"]
-
 intents = discord.Intents.default()
+intents.members = True  # Botun kullanıcılarla çalışmasına ve onları banlamasına izin verir
 intents.message_content = True
 
-client = commands.Bot(command_prefix='!', intents=intents)
+helloMessages = ("howdy", "hi", "hello", "yo")
 
-@client.event
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'Giriş yapıldı:  {bot.user.name}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.command()
+async def start(ctx):
+    await ctx.send(random.choice(helloMessages))
 
-    if message.content.startswith('!'):
-        if message.content.startswith('!hello'):
-            await message.reply(f'{random.choice(helloMessages)} {message.author.mention}')
+@bot.command()
+async def choose(ctx, *choices):
+    await ctx.reply(random.choice(choices[1:]))
 
-        if message.content.startswith('!repeat'):
-            splitmessage = message.content.split()
-            
-            for i in range(int(splitmessage[-1])):
-                await message.channel.send(" ".join(splitmessage[1:-1]))
+@bot.command()
+async def repeat(ctx, amount, *phrase):
+    phrase = " ".join(phrase)
+    for i in range(int(amount)):
+        await ctx.send(phrase)
 
-        if message.content.startswith('!choose'):
-            options = message.content.split()
-            await message.reply(random.choice(options[1:]))
+@bot.command()
+async def addition(ctx, *nums):
+    total = 0
+
+    for j in nums:
+        total += int(j)
+    await ctx.reply(total)
+
+@bot.command()
+async def deduction(ctx, x, *y):
+    main = int(x)
+
+    for j in y:
+        main -= int(j)
+    await ctx.reply(main)
+
+
+@bot.command()
+@commands.has_permissions(ban_members=True)
+async def ban(ctx, member: discord.Member = None):
+    if member:
+        if ctx.author.top_role <= member.top_role:
+            await ctx.send("no")
+        else:
+            await ctx.guild.ban(member)
+            await ctx.send(f"banned {member.name}")
     else:
-        await message.reply(message)
+        await ctx.send("wrong format")
 
-client.run('token')
+@ban.error
+async def ban_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send("no permissions")
+    elif isinstance(error, commands.MemberNotFound):
+        await ctx.send("no user found")
